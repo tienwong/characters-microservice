419b6837514f197bab8ea652a6b3ae5f9f856d1e
Tien <45297291+tienwong@users.noreply.github.com>
2024-05-27 00:15:46 -0500

    Added character routes which weren't included by mistake

---
+++ routes/characterRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mockData = require('../mockData/characters.json')
+
+const router = express.Router({ mergeParams: true })
+
+router.get('/', (req, res) => {
+    const characters = mockData.filter(char => char.seriesId === Number(req.params.seriesId))
+    if (characters.length === 0) {
+        res.status(404).json({
+            message: "No characters were found with the specified series ID."
+        })
+    }
+    res.status(200).json({
+        characters
+    })
+})
+
+router.get('/:characterId', (req, res) => {
+    res.send('Hello from GET /:characterId route of characters!')
+})
+
+router.post('/', (req, res) => {
+    res.send('Hello from POST / route of characters!')
+})
+
+router.put('/:characterId', (req, res) => {
+    res.send('Hello from GET /:characterId route of characters!')
+})
+
+router.delete('/:characterId', (req, res) => {
+    res.send('Hello from GET /:characterId route of characters!')
+})
+
+module.exports = router
