0799af245a7f4da6bb135bd17d7115e42a039080
Tien <45297291+tienwong@users.noreply.github.com>
2024-06-01 19:46:54 -0500

    Add controller for characters POST / route

--- controllers/characterControllers.js
+++ controllers/characterControllers.js
@@ -1,9 +1,17 @@
 const mockData = require('../mockData/characters.json')
 
+const Character = require('../models/Character')
+
 const getByCharacterId = (characterId) => {
     return mockData.find(char => char.characterId === characterId)
 }
 
+const createNewCharacter = (username, seriesId) => {
+    const newCharacter = new Character(username, seriesId)
+    mockData.push(newCharacter)
+}
+
 module.exports = {
-    getByCharacterId
+    getByCharacterId,
+    createNewCharacter
 }
